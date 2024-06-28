4d329b2073c2bde27f7e4046eb0ca3dd5235eeac
Gabriel Nathan <121491867+nythan5@users.noreply.github.com>
2024-06-28 20:09:19 -0300

    atualizando model

--- machines/models.py
+++ machines/models.py
@@ -3,16 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-MODALITIES_LIST = [
-    ('HR', 'Home Repair'),
-    ('RI', 'Reparo Interno'),
-    ('TI', 'Trade-In')
-]
-
 
 class Modalitie(models.Model):
-    name = models.CharField(choices=MODALITIES_LIST,
-                            null=False, max_length=100)
+    name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     user_create = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
